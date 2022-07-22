4559215315a7c852afa17f474fddeab06b24d61a
Brayam1993 <argofrog@live.com.mx>
2022-07-21 23:52:46 -0500

    metric units

--- src/app/weather.js
+++ src/app/weather.js
@@ -6,7 +6,7 @@ export class Weather {
     }
 
     async getWeather() {
-        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}`
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
         const response =await fetch(URI);
         const data = await response.json();
         return data;
