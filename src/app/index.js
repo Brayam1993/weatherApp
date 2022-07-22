3f233ea7a64ed99bd9021bd4fad69bd8dcb52f41
Brayam1993 <argofrog@live.com.mx>
2022-07-22 00:10:03 -0500

    styles

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,15 @@
 const { Weather } = require('./weather');
+const { UI } = require('./ui')
+
+const ui = new UI();
 const weather = new Weather('London','uk')
+
 require('./index.css');
 
 async function fetchWeather() {
     const data = await weather.getWeather();
     console.log(data);
+    ui.render(data)
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeather)
